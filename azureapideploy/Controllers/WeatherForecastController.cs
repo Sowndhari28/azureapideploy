a85e8cc5d65180ed0a1965b80310a1a37a730c24
sowndhari.l <sowndhari.l@saint-gobain.com>
2023-12-02 11:59:00 +0530

    nameadded

--- azureapideploy/Controllers/WeatherForecastController.cs
+++ azureapideploy/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace azureapideploy.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","sowndhari"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
